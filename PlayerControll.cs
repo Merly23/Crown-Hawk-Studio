912a58b23e0dce0b1d0b0c939168c461bd382e0f
xSilentiumx <51062369+xSilentiumx@users.noreply.github.com>
2019-05-29 16:50:13 +0200

    Character Controll(Silentium)
    These are the character controls I've done so far. The values ​​such as jump height, running speed still have to be adjusted. This is for that @Louuiiiss has an idea how far I am.

---
+++ PlayerControll.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    public float gravity;
+    public float runSpeed = 34F;
+    public float walkSpeed = 2F;
+    public float crouchSpeed = 1F;
+    public float jumpPower = 1F;
+    public float jumpDistance = 2.5F;
+    public float rollDistance = 3F;
+    public float rollSpeedMin = 10F;
+    public float rollSpeedMax = 15F;
+    public bool canRun;
+    public bool isDogde;
+    public bool isJump;
+
+    //Realworld//
+    public bool inRealworld;
+    private float gravityRW = 9.81F;
+    private float runSpeedRW = 24F;
+    private float walkSpeedRW = 1F;
+    private float crouchSpeedRW = 0.4F;
+    private float jumpPowerRW = 140F;
+    private float jumpDistanceRW = 1F;
+    private float rollDistanceRW = 2F;
+    private float rollSpeedMinRW = 5F;
+    private float rollSpeedMaxRW = 10F;
+
+    //Dreamworld//
+    public bool inDreamworld;
+    private float gravityDW = 6.8F;
+    private float runSpeedDW = 34F;
+    private float walkSpeedDW = 2F;
+    private float crouchSpeedDW = 1F;
+    private float jumpPowerDW = 260F;
+    private float jumpDistanceDW = 2.5F;
+    private float rollDistanceDW = 3F;
+    private float rollSpeedMinDW = 10F;
+    private float rollSpeedMaxDW = 15F;
+
+    //Private
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        //Connections
+        controller = GetComponent<CharacterController>();
+        //Initialisieren
+        canRun = true;
+        PlayerGoesRealWorld();
+    }
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // Walk
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), -1, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * walkSpeed;
+            controller.Move(moveDirection * Time.deltaTime);
+            //Dogde
+            if (Input.GetButtonDown("Dogde"))
+            {
+                if (!isDogde)
+                {
+                    Debug.Log("hhs");
+                    isDogde = true;
+                }
+                else
+                {
+                    isDogde = false;
+                }
+                UpdateCrouching();
+            }
+            //Jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                isJump = true;
+                moveDirection.y = jumpPower;
+                controller.Move(moveDirection * Time.deltaTime);
+                //JumpAnimation
+                moveDirection = new Vector3(0, -1, jumpDistance);
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection = moveDirection * walkSpeed;
+                controller.Move(moveDirection * Time.deltaTime);
+            }
+            //Sprint
+            if (canRun)
+            {
+                if (Input.GetButton("Run"))
+                {
+                    moveDirection = new Vector3(Input.GetAxis("Horizontal"), -1, Input.GetAxis("Vertical"));
+                    moveDirection = transform.TransformDirection(moveDirection);
+                    moveDirection = moveDirection * runSpeed;
+                    controller.Move(moveDirection * Time.deltaTime);
+                }
+            }
+            isJump = false;
+        }
+        else
+        {
+            //Fall
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+    }
+    //Player enters Dreamworld//
+    public void PlayerGoesDreamWorld()
+    {
+        inDreamworld = true;
+        inRealworld = false;
+        gravity = gravityDW;
+        runSpeed = runSpeedDW;
+        walkSpeed = walkSpeedDW;
+        crouchSpeed = crouchSpeedDW;
+        jumpPower = jumpPowerDW;
+        jumpDistance = jumpDistanceDW;
+        rollDistance = rollDistanceDW;
+        rollSpeedMin = rollSpeedMinDW;
+        rollSpeedMax = rollSpeedMaxDW;
+        isDogde = false;
+        UpdateCrouching();
+    }
+    //Player enters Realworld//
+    public void PlayerGoesRealWorld()
+    {
+        inRealworld = true;
+        inDreamworld = false;
+        gravity = gravityRW;
+        runSpeed = runSpeedRW;
+        walkSpeed = walkSpeedRW;
+        crouchSpeed = crouchSpeedRW;
+        jumpPower = jumpPowerRW;
+        jumpDistance = jumpDistanceRW;
+        rollDistance = rollDistanceRW;
+        rollSpeedMin = rollSpeedMinRW;
+        rollSpeedMax = rollSpeedMaxRW;
+        isDogde = false;
+        UpdateCrouching();
+    }
+    private void UpdateCrouching()
+    {
+        if(isDogde)
+        {
+            walkSpeed = crouchSpeed;
+            //Change WalkAnim to CrouchAnim
+        }
+        else
+        {
+            if (inDreamworld)
+            {
+                walkSpeed = walkSpeedDW;
+            }
+            else
+            {
+                walkSpeed = walkSpeedRW;
+            }
+            //Change CrouchAnim to WalkAnim
+        }
+    }
+}
---
+++ ThirdPersonCameraa.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraa : MonoBehaviour
+{
+    //Public//
+    public Transform cameraTransform;
+    public GameObject Maincamera;
+    // height of camera
+    public float height = 4.0F;
+    //
+
+    //Private//
+    private Transform _target;
+    private Vector3 headOffset = Vector3.zero;
+    private Vector3 centerOffset = Vector3.zero;
+    private bool snap = false;
+    private PlayerControll controller;
+    //x,z distance
+    private float distance = 7.0F;
+    //
+    private float angularSmoothLag = 0.3F;
+    private float angularMaxSpeed = 15.0F;
+    private float heightSmoothLag = 0.3F;
+    private float snapSmoothLag = 0.2F;
+    private float snapMaxSpeed = 720.0F;
+    private float clampHeadPositionScreenSpace = 0.75F;
+    private float lockCameraTimeout = 0.2F;
+    private float heightVelocity = 0.0F;
+    private float angleVelocity = 0.0F;
+    private float targetHeight = 100000.0F;
+
+    void Awake()
+    {
+        if (!cameraTransform && Camera.main)
+            cameraTransform = Camera.main.transform;
+        if (!cameraTransform)
+        {
+            Debug.Log("Assign Camera");
+            enabled = false;
+        }
+
+
+        _target = transform;
+        if (_target)
+        {
+            controller = _target.GetComponent<PlayerControll>();
+        }
+
+        if (controller)
+        {
+            CharacterController characterController = _target.GetComponent<CharacterController>();
+            centerOffset = characterController.bounds.center - _target.position;
+            headOffset = centerOffset;
+            headOffset.y = characterController.bounds.max.y - _target.position.y;
+        }
+        Cut(_target, centerOffset);
+    }
+    public float AngleDistance(float a, float b)
+    {
+        a = Mathf.Repeat(a, 360);
+        b = Mathf.Repeat(b, 360);
+        return Mathf.Abs(b - a);
+    }
+
+    public void Apply(Transform dummyTarget, Vector3 dummyCenter)
+    {
+        // When no target
+        if (!controller)
+        {
+            return;
+        }
+        var targetCenter = _target.position + centerOffset;
+        var targetHead = _target.position + headOffset;
+        // Calculate rotation angle
+        var originalTargetAngle = _target.eulerAngles.y;
+        var currentAngle = cameraTransform.eulerAngles.y;
+        // Adjust angle
+        var targetAngle = originalTargetAngle;
+        // When pressing Fire2 (alt) the camera will snap to the target direction real quick.
+        if (Input.GetButton("Fire2"))
+        {
+            snap = true;
+        }
+        if (snap)
+        {
+            if (AngleDistance(currentAngle, originalTargetAngle) < 3.0)
+            {
+                snap = false;
+            }
+            currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref angleVelocity, snapSmoothLag, snapMaxSpeed);
+        }
+        else
+        {
+            targetAngle = currentAngle;
+            if (AngleDistance(currentAngle, targetAngle) > 160)
+            {
+                targetAngle += 180;
+            }
+            currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle, ref angleVelocity, angularSmoothLag, angularMaxSpeed);
+        }
+        if (controller.isJump)
+        {
+            var newTargetHeight = targetCenter.y + height;
+            if (newTargetHeight < targetHeight || newTargetHeight - targetHeight > 5)
+                targetHeight = targetCenter.y + height;
+        }
+        else
+        {
+            targetHeight = targetCenter.y + height;
+        }
+        var currentHeight = cameraTransform.position.y;
+        currentHeight = Mathf.SmoothDamp(currentHeight, targetHeight, ref heightVelocity, heightSmoothLag);
+        var currentRotation = Quaternion.Euler(0, currentAngle, 0);
+        cameraTransform.position = targetCenter;
+        cameraTransform.position += currentRotation * Vector3.back * distance;
+        SetUpRotation(targetCenter, targetHead);
+    }
+    void LateUpdate()
+    {
+        Apply(transform, Vector3.zero);
+    }
+    void Cut(Transform dummyTarget, Vector3 dummyCenter)
+    {
+        var oldHeightSmooth = heightSmoothLag;
+        var oldSnapMaxSpeed = snapMaxSpeed;
+        var oldSnapSmooth = snapSmoothLag;
+        snapMaxSpeed = 10000F;
+        snapSmoothLag = 0.001F;
+        heightSmoothLag = 0.001F;
+        snap = true;
+        Apply(transform, Vector3.zero);
+        heightSmoothLag = oldHeightSmooth;
+        snapMaxSpeed = oldSnapMaxSpeed;
+        snapSmoothLag = oldSnapSmooth;
+    }
+    void SetUpRotation(Vector3 centerPos, Vector3 headPos)
+    {
+        var cameraPos = cameraTransform.position;
+        var offsetToCenter = centerPos - cameraPos;
+        var yRotation = Quaternion.LookRotation(new Vector3(offsetToCenter.x, 0, offsetToCenter.z));
+        var relativeOffset = Vector3.forward * distance + Vector3.down * height;
+        cameraTransform.rotation = yRotation * Quaternion.LookRotation(relativeOffset);
+        var centerRay = cameraTransform.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 1));
+        var topRay = cameraTransform.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, clampHeadPositionScreenSpace, 1F));
+        var centerRayPos = centerRay.GetPoint(distance);
+        var topRayPos = topRay.GetPoint(distance);
+        var centerToTopAngle = Vector3.Angle(centerRay.direction, topRay.direction);
+        var heightToAngle = centerToTopAngle / (centerRayPos.y - topRayPos.y);
+        var extraLookAngle = heightToAngle * (centerRayPos.y - centerPos.y);
+        if (extraLookAngle < centerToTopAngle)
+        {
+            extraLookAngle = 0;
+        }
+        else
+        {
+            extraLookAngle = extraLookAngle - centerToTopAngle;
+            cameraTransform.rotation *= Quaternion.Euler(-extraLookAngle, 0, 0);
+        }
+    }
+    Vector3 GetCenterOffset()
+    {
+        return centerOffset;
+    }
+}
